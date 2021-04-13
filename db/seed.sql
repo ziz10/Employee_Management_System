97371a39e16a34e6eb756de4824caf51f16325e9
Abdoul Bass <malickya8891@gmail.com>
2021-04-13 10:57:04 -0700

    worked on seed.sql file for mock up purposes

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+use my_cms;
+
+insert into department(name)
+values
+("QA"), 
+("sales")
+("engineering");
+
+insert into role(title, salary, department_id)
+values
+("sales person", 50000, 2)
+("unit tester", 80000, 1)
+("developer", 90000, 3)
