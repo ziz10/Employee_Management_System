bfc3b2df5c99506218dc20cb3ba80621fbc07129
Abdoul Bass <malickya8891@gmail.com>
2021-04-14 12:48:57 -0700

    created remove employee function

--- index.js
+++ index.js
@@ -174,4 +174,26 @@ require("console.table");
       
         loadMainPrompts();
       }
+      async function removeEmployee() {
+        const employees = await db.findAllEmployees();
+      
+        const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { employeeId } = await prompt([
+          {
+            type: "list",
+            name: "employeeId",
+            message: "Which employee do you want to remove?",
+            choices: employeeChoices
+          }
+        ]);
+      
+        await db.removeEmployee(employeeId);
+      
+        console.log("Removed employee from the database");
+      
+        loadMainPrompts();
       
