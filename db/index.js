65f15c1f87c59ad36aca755d64992d8c56f66d3d
Abdoul Bass <malickya8891@gmail.com>
2021-04-14 12:30:41 -0700

    created switch cases depending on user choices

--- db/index.js
+++ db/index.js
@@ -81,4 +81,35 @@ require("console.table");
         ]
       }
     ]);
-  
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+          return viewEmployees();
+        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+          return viewEmployeesByDepartment();
+        case "VIEW_EMPLOYEES_BY_MANAGER":
+          return viewEmployeesByManager();
+        case "ADD_EMPLOYEE":
+          return addEmployee();
+        case "REMOVE_EMPLOYEE":
+          return removeEmployee();
+        case "UPDATE_EMPLOYEE_ROLE":
+          return updateEmployeeRole();
+        case "UPDATE_EMPLOYEE_MANAGER":
+          return updateEmployeeManager();
+        case "VIEW_DEPARTMENTS":
+          return viewDepartments();
+        case "ADD_DEPARTMENT":
+          return addDepartment();
+        case "REMOVE_DEPARTMENT":
+          return removeDepartment();
+        case "VIEW_ROLES":
+          return viewRoles();
+        case "ADD_ROLE":
+          return addRole();
+        case "REMOVE_ROLE":
+          return removeRole();
+        default:
+          return quit();
+      }
+    }
+    
