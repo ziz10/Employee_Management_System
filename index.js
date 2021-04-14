3d5a3298eae96d3a417dd40f63ab34af33967621
Abdoul Bass <malickya8891@gmail.com>
2021-04-14 12:53:11 -0700

    created a function updateEmployeeRole()

--- index.js
+++ index.js
@@ -196,4 +196,44 @@ require("console.table");
         console.log("Removed employee from the database");
       
         loadMainPrompts();
+      }
+  
+      async function updateEmployeeRole() {
+        const employees = await db.findAllEmployees();
       
+        const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { employeeId } = await prompt([
+          {
+            type: "list",
+            name: "employeeId",
+            message: "Which employee's role do you want to update?",
+            choices: employeeChoices
+          }
+        ]);
+      
+        const roles = await db.findAllRoles();
+      
+        const roleChoices = roles.map(({ id, title }) => ({
+          name: title,
+          value: id
+        }));
+      
+        const { roleId } = await prompt([
+          {
+            type: "list",
+            name: "roleId",
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleChoices
+          }
+        ]);
+      
+        await db.updateEmployeeRole(employeeId, roleId);
+      
+        console.log("Updated employee's role");
+      
+        loadMainPrompts();
+      }
